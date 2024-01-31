9595006d858dad30b40f4270d59aaeab228ac448
Mari-Mari <119715631+MariMari01@users.noreply.github.com>
2024-01-30 23:30:21 -0800

    Create main.cpp

---
+++ assignment3/main.cpp
@@ -0,0 +1,24 @@
+/* 
+Author: Samuel Garcia Lopez
+Date: 1.29.2024
+
+This program represents a queue. New elements are added to the back of the queue, and elelments are
+taken out from the front of th queue. 
+ */ 
+#include <iostream>
+#include "queue.hpp"
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "----------------------------------------------------" << endl <<
+           "|                                                    |"
+           "|  Welcome! This program will demonstrate a Queue    |" 
+           "|                                                    |"
+           "|    [1] Add Node to Queue                           |"
+           "|    [2] Remove Node from Queue                      |"
+           "|                                                    |" 
+           "|                                                    |"
+            "----------------------------------------------------" 
+}
