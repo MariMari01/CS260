4ae0fd366b4463c8f088669e993342c8be265e22
Mari-Mari <119715631+MariMari01@users.noreply.github.com>
2024-02-06 23:54:02 -0800

    Update linked_list.hpp

--- assignment_4/linked_list.hpp
+++ assignment_4/linked_list.hpp
@@ -1,1 +1,22 @@
+#include <string>
+/* Initiates Node class */
+class Node
+{
+    public:
+        Node();
+        int ID;
+        int data;
+        Node* next;
+        std::string to_string();
+};
 
+class linked_list
+{
+    Node *new_node;
+public:
+    linked_list();
+    ~linked_list();
+    void add(int value, int position);
+    int remove(int position);
+    int get(int position):
+};
