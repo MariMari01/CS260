4fb9e4a9fe5c7075edddbb296e8fcb2ab124ff97
Mari-Mari <119715631+MariMari01@users.noreply.github.com>
2024-02-16 00:17:16 -0800

    Update auto_sorted_list.cpp

--- assigntment_5/auto_sorted_list.cpp
+++ assigntment_5/auto_sorted_list.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "auto_ordered_list.hpp"
+#include "auto_sorted_list.hpp"
 using std::string;
 
 /* --------------------------------------------- NODE DEFINITION --------------------------------------------------- */
@@ -21,7 +21,7 @@ string Node::to_string()
 }
 /* -------------------------------------------- LINKED LIST DEFINITION -------------------------------------------- */
 
-Auto_ordered_list::Auto_ordered_list()
+Auto_sorted_list::Auto_sorted_list()
 {
     this->head = nullptr;
     this->tail = nullptr;
@@ -30,12 +30,12 @@ Auto_ordered_list::Auto_ordered_list()
 
 
 /* Adds a new Node to the list in order */
-void Auto_ordered_list::insert(int value)
+void Auto_sorted_list::insert(int value)
 {
     Node *new_node = new Node;
     new_node->data = value;
 
-    //If list is empty, the new node will be the head.
+    //If list is empty, the new node will be the head and tail.
     if (this->head == nullptr && this->tail == nullptr)
     {
         this->head = new_node;
@@ -43,14 +43,14 @@ void Auto_ordered_list::insert(int value)
     } 
     
     //If the node's value is larger than the tail, it becomes the new tail.
-    else if (new_node->data > this->tail->data)
+    else if (new_node->data >= this->tail->data)
     {
         this->tail->next = new_node;
         this->tail = new_node;
     }
 
     //If the node's value is smaller than the head's
-    else if (new_node->data < this->head->data)
+    else if (new_node->data <= this->head->data)
     {
         new_node->next = this->head;
         this->head = new_node;
@@ -61,55 +61,76 @@ void Auto_ordered_list::insert(int value)
     {   
         //Seach for where to insert the node
         Node *temp_node = this->head;
-        while (temp_node->data <= new_node->data && temp_node->next != nullptr)
+        Node *previous = nullptr;
+        while (temp_node->data < new_node->data && temp_node->next != nullptr)
         {
+            previous = temp_node;
             temp_node = temp_node->next;
         }
-        new_node->next = temp_node->next;
-        temp_node->next = new_node;
-
+        new_node->next = previous->next;
+        previous->next = new_node;
     }
 }
 
 
-/* Deletes all nodes with a certain value from the list. */
-void Auto_ordered_list::remove(int value)
+/* Deletes the first instance of a node with the given value and returns its position. Returns -1 if it is not found. */
+int Auto_sorted_list::remove(int value)
 {
+    int position = 0;
     Node *temp_node;
     temp_node = this->head;
     Node *previous;
     
-    //Begin searching for the node.
-    while (temp_node != nullptr)
+    //If the head is the node to be deleted.
+    if (this->head->data == value)
     {
+        //If the list is only one node long, also set tail to nullptr.
+        if (this->head == this->tail) 
+            this->tail = this->tail->next;
+        this->head = this->head->next;
+        delete temp_node;
+        return position;
+    }
+    else
+    {
+        //Begin searching for the node.
+        while (temp_node->data != value && temp_node->next != nullptr)
+        {   
+            position++;
+            previous = temp_node;
+            temp_node = temp_node->next;
+
+        }
+        //If we found the value
         if (temp_node->data == value)
-        {
-            if (temp_node == this->head)
+        {          
+            //If the first deleted node is the tail
+            if (temp_node->data == this->tail->data)
             {
-                this->head = temp_node->next;
+                this->tail = previous;
+                this->tail->next = temp_node->next;
                 delete temp_node;
-                temp_node = this->head;
+                return position;
             }
             else
             {
                 previous->next = temp_node->next;
                 delete temp_node;
-                temp_node = previous->next;
+                return position;
             }
         }
-        previous = temp_node;
-        temp_node = temp_node->next;       
-    }
+        return -1;  
+    }   
 }
 
 
 /* Returns the amount of times a value shows up in the linked list. */
-int Auto_ordered_list::search(int value)
+int Auto_sorted_list::search(int value)
 {
     int count = 0;
     Node *temp_node = this->head;
 
-    while (temp_node != nullptr)
+    while (temp_node->next != nullptr)
     {
         if (temp_node->data == value)
             count++;
@@ -120,7 +141,7 @@ int Auto_ordered_list::search(int value)
 
 
 //Displays all values present within the linked list.
-void Auto_ordered_list::display()
+void Auto_sorted_list::display()
 {
     Node *temp = this->head;
     while (temp != nullptr)
@@ -131,7 +152,7 @@ void Auto_ordered_list::display()
 }
 
 //Deletes the linked list.
-Auto_ordered_list::~Auto_ordered_list()
+Auto_sorted_list::~Auto_sorted_list()
 {
     Node *temp;
     while (this->head != nullptr)
@@ -140,4 +161,5 @@ Auto_ordered_list::~Auto_ordered_list()
         this->head = this->head->next;
         delete temp;
     }
+    this->tail = nullptr;
 }
