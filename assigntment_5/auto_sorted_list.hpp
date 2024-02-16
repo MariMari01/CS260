69ce1e8c3a9850c7c150d729c1b2695a20ce7f2c
Mari-Mari <119715631+MariMari01@users.noreply.github.com>
2024-02-15 20:06:46 -0800

    Update auto_sorted_list.hpp

--- assigntment_5/auto_sorted_list.hpp
+++ assigntment_5/auto_sorted_list.hpp
@@ -1,1 +1,25 @@
+#include <string>
+/* Initiates Node class */
+class Node
+{
+    public:
+        Node();
+        int position;
+        int data;
+        Node* next;
+        std::string to_string();
+};
 
+class Auto_ordered_list
+{
+    Node *head;
+    Node *tail; 
+
+public:
+    Auto_ordered_list();
+    ~Auto_ordered_list();
+    void insert(int value);
+    int remove(int value);
+    int search(int value);
+    void display();
+};
