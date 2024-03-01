5c6c730b5a7df318dcdf1cda5a502ea9365dcfdd
Mari-Mari <119715631+MariMari01@users.noreply.github.com>
2024-03-01 00:55:20 -0800

    Update main.cpp

--- assignment_6/main.cpp
+++ assignment_6/main.cpp
@@ -1,1 +1,61 @@
+#include <iostream>
+#include "binary_tree.hpp"
+#include "utility.hpp"
+using std::cout;
+using std::endl;
+/*
+Author: Samuel Garcia Lopez
+Date: 2.29.24
+*/
 
+int main()
+{
+    Binary_tree tree;
+    bool exit = false;
+    int choice, data;
+    enum choices{add = 1, remove, in_order, breadth_first, quit};
+
+    do
+    {
+        show_menu();
+        choice = get_int();
+        //User choice
+        switch (choice)
+        {
+            case add:
+            {
+                cout << "What value will this node have?" << endl;
+                data = get_int();
+                tree.add(data);
+                break;
+            }
+            case remove:
+            {
+                cout << "Remove what value?" << endl;
+                data = get_int();
+                tree.remove(data);
+                break;   
+            }
+            case in_order:
+            {
+                tree.io_traversal();
+                break;
+            }
+            case breadth_first:
+            {
+                break;
+            }
+            case quit:
+            {
+                exit = true;
+                break;  
+            }
+            default:
+            {
+                cout << "Error - Wrong input" << endl;
+                break;
+            }
+        }
+    } while (exit == false);
+    return 0;
+}
