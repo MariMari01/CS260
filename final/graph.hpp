512bcb7548ae0b064ed92b941cce70ad3add0306
Mari-Mari <119715631+MariMari01@users.noreply.github.com>
2024-03-21 00:15:40 -0700

    Update graph.hpp

--- final/graph.hpp
+++ final/graph.hpp
@@ -1,16 +1,40 @@
-class graph
+#pragma once
+#include <vector>
+#include <string>
+#include "vertex.hpp"
+#include "edge.hpp"
+
+using std::vector;
+using std::string;
+
+class Graph
 {
-private:
-    /* data */
-public:
-    graph(/* args */);
-    ~graph();
+    private:
+        int size;
+        vector<Vertex*> vertices;
+        vector<Edge*> edges;
+    public:
+        Graph();
+        ~Graph();
+
+        void add_vertex(string name, int value);
+        
+        void add_edge(string vertex_1, string vertex_2, int weight);
+        Vertex* find_vertext(string vertex_name);
+        
+        int shortest_path(string begining, string ending);
+        int edge_weight(string vertex_1, string vertex_2);
+
+        void minimum_span_tree();
+
+
 };
 
-graph::graph(/* args */)
+Graph::Graph()
 {
+    this->size = 0;
 }
 
-graph::~graph()
+Graph::~Graph()
 {
 }
