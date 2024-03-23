63db3a9d8c735f3e7c6781a07a300ba55f254a2b
Mari-Mari <119715631+MariMari01@users.noreply.github.com>
2024-03-22 21:22:41 -0700

    Update graph.hpp

--- final/graph.hpp
+++ final/graph.hpp
@@ -17,26 +17,21 @@ class Graph
     public:
         Graph();
         ~Graph();
+        const int INFINITY = 2147483647;
 
         void add_vertex(string name, int value);
         
         void add_edge(string vertex_1, string vertex_2, int weight);
         Vertex* find_vertext(string vertex_name);
-        
+        void display_vertices();
+
         int shortest_path(string begining, string ending);
+        int nearest(vector<int> dist, vector<bool>searched);
         int edge_weight(string vertex_1, string vertex_2);
-        int find_dist(vector<pair<)
+        void display_edges();
 
         void minimum_span_tree();
 
-
 };
 
-Graph::Graph()
-{
-    this->size = 0;
-}
 
-Graph::~Graph()
-{
-}
